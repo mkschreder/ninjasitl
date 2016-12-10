a031b16af9e8b6a427ef1d84a4df72a1bd0760c3
Martin Schröder <mkschreder.uk@gmail.com>
2016-12-10 16:51:53 +0100

    update startup script

--- start-quadsim.sh
+++ start-quadsim.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-killall quadcopter
+set -e
+
+killall quadcopter || echo "No process found!"
 
 if [ ! -d build ]; then 
 	mkdir build; 
 fi
 
+# download pak0
+if [ ! -f "base/pak0.pk3" ]; then
+	echo "Downloading pak0..."
+	wget http://files.anitalink.com/gamecache/quake3/baseq3/pak0.pk3 -O base/pak0.pk3 || {
+		echo "Please download pak0.pk3 file from somewhere. It is part of quake3. It contains standard quake textures that you will need to view maps."
+	}
+fi
+
 cd build
-cmake ..
+cmake ../src
 cd ..
 
 make -C build -j8 && ./build/quadcopter #-m Q3AnthraxDM1.bsp
